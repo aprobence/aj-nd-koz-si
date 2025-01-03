9e3f8cffde27769eb9fd6a36055d4077c177b507
aprobence <aprobence2017@gmail.com>
2025-01-03 17:56:05 +0100

    próba

--- ajandekozas/Program.cs
+++ ajandekozas/Program.cs
@@ -5,5 +5,9 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello, World!");
+        for (int i = 0; i < 10; i++)
+        {
+            Console.WriteLine(i);
+        }
     }
 }
